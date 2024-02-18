cbd47887fa49e1a27c969b23b94a32874bd8896a
Elvin <elvinva2271@gmail.com>
2024-02-18 20:31:51 +0530

    changed UserComplaint

--- client/src/UserComplaint.jsx
+++ client/src/UserComplaint.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDisconnect } from "@thirdweb-dev/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ConnectWallet } from "@thirdweb-dev/react";
 import {
   faCamera,
   faFileAlt,
   faMapMarkerAlt,
-} from "@fortawesome/free-solid-svg-icons"; // Added necessary FontAwesome icons
+} from "@fortawesome/free-solid-svg-icons";
 import { UploadOutlined } from "@ant-design/icons";
 import { Button, message, Upload } from "antd";
 
 const UserComplaint = () => {
+  const [location, setLocation] = useState("");
+  const [details, setDetails] = useState("");
   const disconnect = useDisconnect();
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleDetailsChange = (event) => {
+    setDetails(event.target.value);
+  };
 
   const props = {
     name: "file",
@@ -40,14 +51,19 @@ const UserComplaint = () => {
 
   return (
     <div className="bg-cover bg-gradient-to-br from-stone-300 from-10% via-zinc-400 via-30% to-zinc-900 to-90% min-h-screen text-gray-800 overflow-hidden">
-      <h1 className="text-4xl font-bold ml-6 text-zinc-800 py-3 uppercase">
-        Safe Whistle
-      </h1>
-      <div className="flex items-center justify-center h-full">
-        <div className="border-2 border-neutral-100 p-6 rounded-lg shadow-2xl w-full max-w-7xl mt-24  bg-opacity-70 backdrop-filter backdrop-blur-md">
+      <div className="flex  lg:flex-row justify-between items-center ">
+        <h1 className="text-4xl font-bold ml-6 text-zinc-800 py-3 uppercase  lg:mt-3">
+          Safe Whistle
+        </h1>
+        <div className=" mt-3  lg:mr-12 ">
+          <ConnectWallet />
+        </div>
+      </div>
+      <div className="flex items-center justify-center  h-full">
+        <div className="border-2 border-neutral-100 p-6 rounded-lg shadow-2xl w-full max-w-7xl mt-14 lg:mt-20 bg-opacity-70 backdrop-filter backdrop-blur-md">
           <form className="rounded-lg p-6">
             <div className="mb-6">
-              <h1 className="text-2xl font-bold mb-4 text-center text-black uppercase">
+              <h1 className="text-3xl font-bold mb-2 text-center text-black uppercase">
                 Complaint Registration
               </h1>
               <label
@@ -78,7 +94,9 @@ const UserComplaint = () => {
               </label>
               <textarea
                 id="details"
-                className="block w-full h-20 px-4 py-2 text-sm text-gray-800 border border-gray-300 rounded-lg bg-slate-300 focus:outline-none focus:ring focus:border-blue-300"
+                className="block w-full h-40 px-4 py-2 text-sm text-gray-800 border border-gray-300 rounded-lg bg-transparent focus:outline-none focus:ring focus:border-blue-300"
+                value={details}
+                onChange={handleDetailsChange}
               />
             </div>
             <div className="mb-6">
@@ -97,8 +115,10 @@ const UserComplaint = () => {
                 <input
                   type="text"
                   id="location"
-                  className="block w-full px-4 py-2 text-sm text-gray-800 border border-gray-300 rounded-lg bg-slate-300 focus:outline-none focus:ring focus:border-blue-300"
+                  className="block w-full px-4 py-2 text-sm text-black border border-gray-300 rounded-lg  focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter location"
+                  value={location}
+                  onChange={handleLocationChange}
                 />
                 <div className="absolute top-1/2 right-4 transform -translate-y-1/2">
                   <svg
@@ -118,15 +138,15 @@ const UserComplaint = () => {
                 </div>
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex flex-col lg:flex-row justify-between">
               <button
                 type="submit"
-                className="px-6 py-3 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 transition duration-300 ease-in-out transform hover:scale-105"
+                className="w-full lg:w-auto mb-4 lg:mb-0 px-6 py-3 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 transition duration-300 ease-in-out transform hover:scale-105"
               >
                 Submit Report
               </button>
               <button
-                className="bg-red-500 text-white font-medium py-3 px-6 rounded-full hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105"
+                className="w-full lg:w-auto bg-red-500 text-white font-medium py-3 px-6 rounded-full hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105"
                 onClick={disconnect}
               >
                 Cancel
