85d7c786b237746c2547cb7d12d0024a78f4294e
arjun-ms <arjunachu123.aa@gmail.com>
2024-03-21 17:36:57 +0530

    Add mumbai testnet

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork:'mumbai',
+    networks:{
+      hardhat:{},
+      mumbai:{
+        url:'https://rpc-mumbai.maticvigil.com',
+        accounts:[`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
